449d48b3bd6990231d1f9625f2dbdceb43e2543d
Raymundo Villegas <memovill@live.com>
2015-02-12 11:07:52 -0800

    6

--- model/Database.php
+++ model/Database.php
@@ -8,11 +8,28 @@ class Database {
     private $password;
     private $database;
 
-    public function __contrust($host, $username, $password, $database) {
+    public function __construct($host, $username, $password, $database) {
         $this->host = $host;
         $this->username = $username;
         $this->password = $password;
         $this->database = $database;
+
+        $this->connection = new mysqli($host, $username, $password);
+
+        if ($this->connection->connect_error) {
+            die("<p>Error: " . $this->connection->connect_error . "</p>");
+        }
+
+        $exists = $this->connection->select_db($database);
+        if (!$exists) {
+            $query = $this->connection->query("CREATE DATABASE $database");
+
+            if ($query) {
+                echo"<p>Sucessfully created database" . $database . "</p>";
+            }
+        } else {
+            echo "<p>Database already exists.</p>";
+        }
     }
 
     public function openConnection() {
@@ -24,19 +41,19 @@ class Database {
     }
 
     public function closeConnection() {
-        if(isset($this->connection)){
-            $this->connection ->close();
+        if (isset($this->connection)) {
+            $this->connection->close();
         }
     }
 
     public function query($string) {
         $this->openConnection();
-        
-         $query = $this->connection->query($string);    
-         
-         $this->closeConnection();
-         
-         return $query;
+
+        $query = $this->connection->query($string);
+
+        $this->closeConnection();
+
+        return $query;
     }
 
 }
--- model/config.php
+++ model/config.php
@@ -1,8 +1,16 @@
 <?php
+require_once(__DIR__ . "/Database.php");
+session_start();
+
 $path = "/villegasr-blog/";
 
 $host = "localhost";
 $username = "root";
 $password  = "root";
 $database = "blog_db";
+
+if(!isset($_SESSION["connection"])){
+    $connection = new Database($host, $username, $password, $database);
+    $_SESSION["connection"] = $connection;
+}
 
